7ac7faf95e31c81c72a991c65f3360d6a1430e2e
yuqi <1913753929@qq.com>
2020-11-13 14:06:34 +0800

    接入

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -119,23 +119,16 @@ class TestController extends Controller
                         $file_type='.jpg';
                         $data["url"] = $obj->PicUrl;
                         $data["media_id"]=$obj->MediaId;
-                    }else
-                    //视频
-                    if($obj->MsgType=="video"){
+                    }elseif($obj->MsgType=="video"){
                         $file_type='.mp4';
                         $data["media_id"]=$obj->MediaId;
-                    }else
-                    //文本
-                    if($obj->MsgType=="text"){
+                    }elseif($obj->MsgType=="text"){
                         $file_type='.txt';
                         $data["content"]=$obj->Content;
-                    }else
-                    //音频
-                    if($obj->MsgType=="voice"){
+                    }elseif($obj->MsgType=="voice"){
                         $file_type=".amr";
                         $data["media_id"]=$obj->MediaId;
-                    }else
-                    if(!empty($file_type)){
+                    }elseif(!empty($file_type)){
                         file_put_contents("dwaw".$file_type,$url);
                     }
                     Messa::insert($data);
